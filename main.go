552abe984dc6d7ce901bd0da37df0801a04f6307
Doan Trong Dat <63988302+TMS-DatDT@users.noreply.github.com>
2022-06-08 14:41:40 +0700

    Add test transfer function

--- main.go
+++ main.go
@@ -16,28 +16,45 @@ type createWalletRequest struct {
 
 type topUpRequest struct {
 	From   string `json:"From"`
-	Amount int    `json:"Amount"`
+	Amount int64  `json:"Amount"`
 }
 
-func TestCreateWallet(name string, email string, phone string) error {
+type transferRequest struct {
+	From   string `json:"From"`
+	To     string `json:"To"`
+	Amount int64  `json:"Amount"`
+}
+
+type createWalletResponse struct {
+	WalletAddress string `json:"WalletAddress"`
+	Error         string `json:"Error"`
+}
+
+func TestCreateWallet(name string, email string, phone string) (string, error) {
+	address := ""
 	reqData := createWalletRequest{name, email, phone}
 	reqJson, _ := json.Marshal(reqData)
 	reqBody := bytes.NewBuffer(reqJson)
 	resp, err := http.Post("http://127.0.0.1:9090/create-wallet", "application/json", reqBody)
 	if err != nil {
 		log.Println(err)
-		return err
+		return address, err
 	}
-	sBody, err := io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
-		return err
+		return address, err
 	}
-	log.Println(string(sBody))
-	return nil
+	sBody := string(body)
+	log.Println(sBody)
+
+	var respJson createWalletResponse
+	json.Unmarshal(body, &respJson)
+	address = respJson.WalletAddress
+	return address, nil
 }
 
-func TestTopUp(address string, amount int) error {
+func TestTopUp(address string, amount int64) error {
 	reqData := topUpRequest{address, amount}
 	reqJson, _ := json.Marshal(reqData)
 	reqBody := bytes.NewBuffer(reqJson)
@@ -55,14 +72,37 @@ func TestTopUp(address string, amount int) error {
 	return nil
 }
 
+func TestTransfer(from string, to string, amount int64) error {
+	reqData := transferRequest{from, to, amount}
+	reqJson, _ := json.Marshal(reqData)
+	reqBody := bytes.NewBuffer(reqJson)
+	resp, err := http.Post("http://127.0.0.1:9090/transfer", "application/json", reqBody)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	sBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	log.Println(string(sBody))
+	return nil
+}
+
 func main() {
-	log.Print("Start Main")
-	//err := TestCreateWallet("DatDT", "datdt@microtecweb.com", "0123456789")
+	//log.Print("Start Main")
+	address, err := TestCreateWallet("Tran Van Huy", "huytv@microtecweb.com", "0123456789")
+	if err != nil {
+		log.Println(err)
+	}
+
+	//err = TestTopUp(address, 100)
 	//if err != nil {
 	//	log.Println(err)
 	//}
 
-	err := TestTopUp("0x482B90D5AAD8340A216b7463860891356986AD5e", 100)
+	err = TestTransfer(address, "0x8Af8364b963092F765DCaAC76813ea4619be43fD", 100)
 	if err != nil {
 		log.Println(err)
 	}
